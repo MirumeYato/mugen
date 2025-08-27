939c46cccbaae45c75da0bd7f599d8398daebc5a
Grigory Vorobyev <gree-han@yandex.ru>
2025-08-27 11:13:02 +0000

    some dummy tests adding

---
+++ lib/utils/tools.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime
+import re
+
+from lib import PATH
+
+def make_unique_output_dir(base_dir: str) -> str:
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+        return base_dir
+    else:
+        timestamp = datetime.now().strftime("ver%y%m%d_%H%M")
+        new_dir = f"{base_dir}_{timestamp}"
+        os.makedirs(new_dir)
+        return new_dir
+    
+def extract_sort_key(name):
+    # Match everything until last number group and capture it
+    match = re.match(r"(.+?)(\d+p?\d*)m", name)
+    if match:
+        prefix, number_str = match.groups()
+        number = float(number_str.replace('p', '.'))
+        return (prefix, number)
+    else:
+        return (name, float('inf'))
+
+def path_debug_test(): 
+    print(PATH)
+    pass
---
+++ tests/dummy_test.py
@@ -0,0 +1,16 @@
+# Path settings
+import os, sys
+#===============================#
+# Get the directory where the script is located
+PATH = os.path.dirname(os.path.abspath(__file__))
+# Get the parent directory of the current directory
+PATH = os.path.abspath(os.path.join(PATH, '..'))
+sys.path.insert(0, PATH)
+#===============================#
+
+from lib.utils import path_debug_test
+
+if __name__ == "__main__":
+    path_debug_test()
+
+
